67a3821ee6eb6105104cb6f4ce469d722050b437
Baranga George Marius <baranga.georgemarius@gmail.com>
2022-10-03 00:29:44 +0300

    snake v1.0

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Snake
+{
+    partial class BSnake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BSnake));
+            this.TabelaDeJoc = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.imglist1 = new System.Windows.Forms.ImageList(this.components);
+            this.imglist = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelaDeJoc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TabelaDeJoc
+            // 
+            this.TabelaDeJoc.Location = new System.Drawing.Point(22, 12);
+            this.TabelaDeJoc.Name = "TabelaDeJoc";
+            this.TabelaDeJoc.Size = new System.Drawing.Size(630, 630);
+            this.TabelaDeJoc.TabIndex = 0;
+            this.TabelaDeJoc.TabStop = false;
+            this.TabelaDeJoc.Click += new System.EventHandler(this.BSnake_Load);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // imglist1
+            // 
+            this.imglist1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglist1.ImageStream")));
+            this.imglist1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imglist1.Images.SetKeyName(0, "1.PNG");
+            this.imglist1.Images.SetKeyName(1, "bonus2.png");
+            this.imglist1.Images.SetKeyName(2, "bonus3.png");
+            this.imglist1.Images.SetKeyName(3, "bonus4.png");
+            this.imglist1.Images.SetKeyName(4, "cap.png");
+            this.imglist1.Images.SetKeyName(5, "corp.png");
+            this.imglist1.Images.SetKeyName(6, "death.png");
+            // 
+            // imglist
+            // 
+            this.imglist.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglist.ImageStream")));
+            this.imglist.TransparentColor = System.Drawing.Color.Transparent;
+            this.imglist.Images.SetKeyName(0, "zid.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Foloseste sagetile pentru a ajuta sarpele sa adune merele.\r\nP.S.: - apasa una din" +
+    " sageti pentru a incepe\r\n         - succes!!! ";
+            // 
+            // BSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(917, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TabelaDeJoc);
+            this.Name = "BSnake";
+            this.Load += new System.EventHandler(this.BSnake_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BSnake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.TabelaDeJoc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox TabelaDeJoc;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ImageList imglist1;
+        private System.Windows.Forms.ImageList imglist;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class BSnake : Form
+    {
+        Random r;  // pentru bonusuri
+        enum campuri
+        {
+            Gol,Snake,Bonus
+        };
+        // lista campuri din tabela(in tabela noastra de joc putem avea 3 tipuri de campuri):
+        // 1. campuri goale , unde nu avem nimic
+        // 2. campurile unde se afla sarpele
+        // 3. campurile unde se afla bonusurile
+
+
+        enum Directii
+        {
+            Sus,Jos,Stanga,Dreapta
+        }; 
+        // lista directii : 4 directii(sus , jos , stanga , dreapta)
+ 
+
+        struct Coordonate
+        {
+            // coordonatele caracterului
+            public int x;
+           public int y;
+        }; 
+
+
+        campuri[,] tabela; //matrice de tip campuri , prin care putem accesa campurile din tabela de joc
+
+        Coordonate[] snakeXY;  // vectori de tip coordonate pentru a memora coordonatele caracterului
+
+        int snakeLungime; // lungimea caracterului pe care o vom modifica la fiecare moment
+        Directii d; // o variabila de tip directii prin care putem accesa directiile 
+
+        Graphics g; // variabila care ne va ajuta sa "pictam" totul in tabela
+
+        public BSnake()
+        {
+            //initializam variabilele
+            InitializeComponent();
+            tabela = new campuri[18, 18];
+            snakeXY = new Coordonate[500];
+            r = new Random();
+        }
+
+        private void BSnake_Load(object sender, EventArgs e)
+        {
+            TabelaDeJoc.Image = new Bitmap(630, 630); // in acest "Bitmap" creat vom "picta" imaginile
+            g = Graphics.FromImage(TabelaDeJoc.Image);
+            g.Clear(Color.White);
+
+            for (int i = 0; i <= 17; i++)
+            {
+                //afisam zidurile de sus si de jos
+                g.DrawImage(imglist.Images[0], i * 35, 0);
+                g.DrawImage(imglist.Images[0], i * 35, 595);
+            }
+
+            for (int i = 0; i <= 18; i++)
+            {
+                //afisam zidurile din stanga si dreapta
+                g.DrawImage(imglist.Images[0], 0, i * 35);
+                g.DrawImage(imglist.Images[0], 595, i * 35);
+            }
+
+
+            // afisam sarpele la coordonatele initiale
+            snakeXY[0].x = 5;
+            snakeXY[0].y = 15; //coordonatele capului
+
+            snakeXY[1].x = 5;
+            snakeXY[1].y = 16;  // coordonatele primei parti din corp
+
+
+
+            g.DrawImage(imglist1.Images[4], 5 * 35, 15 * 35);
+            g.DrawImage(imglist1.Images[5], 5 * 35, 16 * 35); //prima parte a corpului pictata pe mapa
+
+
+            tabela[5, 15] = campuri.Snake;
+            tabela[5, 16] = campuri.Snake;
+
+            d = Directii.Sus;  //pornim initial in sus
+            snakeLungime = 2;  //sarpele are initial doar capul si o singura parte din corp
+
+
+            Bonus();
+        }
+        //"pictam" bonusurile pe mapa 
+        private void Bonus()
+        {
+            int x, y;
+
+            do
+            {
+                x = r.Next(1, 17);
+                y = r.Next(1, 17);
+                //coordonatele random din mapa
+            } while (tabela[x, y] != campuri.Gol); // verificam daca bonusul pica pe un camp liber
+
+            tabela[x, y] = campuri.Bonus;
+            g.DrawImage(imglist1.Images[0], x * 35, y * 35);
+        }
+
+        // setam directiile pe care se va misca sarpele
+        private void BSnake_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:               
+                    timer.Enabled = true;
+                    d = Directii.Sus;
+                    break;                          
+                case Keys.Down:
+                    timer.Enabled = true;
+                    d = Directii.Jos;
+                    break;
+                case Keys.Right:
+                    timer.Enabled = true;
+                    d = Directii.Dreapta;
+                    break;
+                case Keys.Left:
+                    timer.Enabled = true;
+                    d = Directii.Stanga;
+                    break;
+            }
+        }
+        private void Sfarsitul_Jocului()
+        {
+            timer.Enabled = false;
+            MessageBox.Show("Joc terminat - Snake - scor:" + snakeLungime);
+            ActiveForm.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //stergem coada caracterului
+            g.FillRectangle(Brushes.White, snakeXY[snakeLungime - 1].x * 35, snakeXY[snakeLungime - 1].y * 35, 35, 35);
+            tabela[snakeXY[snakeLungime - 1].x, snakeXY[snakeLungime - 1].y] = campuri.Gol;
+
+            //misca caracterul pe pozitia din urma
+            for (int i = snakeLungime; i >= 1; i--)
+            {
+                snakeXY[i].x = snakeXY[i - 1].x;
+                snakeXY[i].y = snakeXY[i - 1].y;
+            }
+            g.DrawImage(imglist1.Images[5], snakeXY[0].x * 35, snakeXY[0].y * 35);
+
+
+            //schimbam directia 
+            switch (d)
+            {
+                case Directii.Sus:
+                    snakeXY[0].y = snakeXY[0].y - 1;
+                    break;
+                case Directii.Jos:
+                    snakeXY[0].y = snakeXY[0].y + 1;
+                    break;
+                case Directii.Stanga:
+                    snakeXY[0].x = snakeXY[0].x - 1;
+                    break;
+                case Directii.Dreapta:
+                    snakeXY[0].x = snakeXY[0].x + 1;
+                    break;
+            }
+
+            // verificam daca lovim zidul
+            if (snakeXY[0].x < 1 || snakeXY[0].x >= 17 || snakeXY[0].y < 1 || snakeXY[0].y >= 17)
+            {
+                g.DrawImage(imglist1.Images[6], snakeXY[0].x * 35, snakeXY[0].y * 35);
+                tabela[snakeXY[0].x, snakeXY[0].y] = campuri.Snake;
+                TabelaDeJoc.Refresh();
+                Sfarsitul_Jocului();
+                return;
+            }
+
+            // verificam daca ne lovim de corpul caracterului
+            if (tabela[snakeXY[0].x, snakeXY[0].y] == campuri.Snake)
+            {
+                g.DrawImage(imglist1.Images[6], snakeXY[0].x * 35, snakeXY[0].y * 35);
+                tabela[snakeXY[0].x, snakeXY[0].y] = campuri.Snake;
+                TabelaDeJoc.Refresh();
+                Sfarsitul_Jocului();
+                return;
+            }
+
+
+            // verificam daca mancam bonusul
+            if (tabela[snakeXY[0].x, snakeXY[0].y] == campuri.Bonus)
+            {
+                Console.Beep(2000, 50);
+                g.DrawImage(imglist1.Images[5], snakeXY[snakeLungime].x * 35, snakeXY[snakeLungime].y * 35);
+                tabela[snakeXY[snakeLungime].x, snakeXY[snakeLungime].y] = campuri.Snake;
+                snakeLungime++;
+
+                if (snakeLungime < 289)
+                    Bonus();
+
+               Text = "Scor - " + snakeLungime;
+            }
+
+
+            // desenam capul caracterului
+            g.DrawImage(imglist1.Images[4], snakeXY[0].x * 35, snakeXY[0].y * 35);
+            tabela[snakeXY[0].x, snakeXY[0].y] = campuri.Snake;
+
+            TabelaDeJoc.Refresh();
+        }
+
+    }
+}
